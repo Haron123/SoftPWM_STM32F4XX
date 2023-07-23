dac6b79497011071f68aa39a6148bc4efff2b6d6
Haron123 <31776616+Haron123@users.noreply.github.com>
2023-07-23 11:21:55 +0200

    Update error_handler.c

--- Src/error_handler.c
+++ Src/error_handler.c
@@ -4,8 +4,6 @@ void error_handler(char* msg, uint32_t msg_len, char* file, uint32_t line)
 {
     // Turn on Error LED
     __disable_irq();
-    gpio_setup_basic(ERROR_LED_PORT, ERROR_LED_PIN, OUTPUT);
-    GPIOC->BSRR = (1 << (ERROR_LED_PIN * 2));
     // TODO : Write msg, but where to ?
     while(1);
 }
